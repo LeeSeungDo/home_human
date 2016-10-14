7a3aece1b00ab1fe599625b3e306593522a47f83
BitCamp <BitCamp@BitCamp-PC>
2016-10-14 19:07:01 +0900

    이승도(민원 최종수정)

--- hh/src/main/webapp/js/gongziInput.js
+++ hh/src/main/webapp/js/gongziInput.js
@@ -18,28 +18,10 @@ function ajaxAddGongzi(gongzi) {
 	    	 alert("등록 실패입니다.")
 	    	 return
 	    }
-		ajaxGongziList();
-	    // window.location.href = "gongziForm.html?no=" + no
+	    window.location.href = serverAddr + "/html/gongzi.html"
 	    
 	}, "json")
 }
-
-function ajaxGongziList() {
-	$.getJSON(serverAddr + "/gongzi/list.json", function(obj) {
-		var result = obj.jsonResult
-		console.log(result)
-		if (result.state != "success") {
-	    	 alert("서버에서 데이터를 가져오는데 실패했습니다.")
-	    	 return
-	    }
-		for (var i = 0; i < 100; i++) {
-			console.log(result.data.no[i]);
-		}
-	    })
-
-}
-
-
 
 
 
