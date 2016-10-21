047d5fd0657ba1af90abf723a38ea2ccddc77402
BitCamp <BitCamp@BitCamp-PC>
2016-10-21 19:18:10 +0900

    .

--- hh/src/main/webapp/js/board/complain_t.js
+++ hh/src/main/webapp/js/board/complain_t.js
@@ -7,7 +7,7 @@ $("#logoutBtn").click(function(event) {
 });*/
 
 $("#addBtn").click(function(event) {
-	var complain = { 
+	var complain = {
 	  title: $("#title").val(),
 	  contents: $("#contents").val(),
 	  writer: $("#writer").val()
@@ -45,7 +45,7 @@ function ajaxComplainList() {
 	    $("#complainTable .king").html(template(result.data))
 	    
 	    $(".titleLink").click(function(event) {
-		    window.location.href = serverAddr + "/html/board/complainForm_t.html?no=" + $(this).attr("data-no")
+		    window.location.href = "complainForm_t.html?no=" + $(this).attr("data-no")
 	    })
 	    
 	    // 현재 페이지 번호를 span 태그에 출력한다.
@@ -77,40 +77,43 @@ function ajaxAddComplain(complain) {
 	    	 alert("등록 실패입니다.")
 	    	 return
 	    } 
-	    window.location.href = serverAddr + "/html/board/complain_t.html"
+	    window.location.href = "complainApp_t.html"
 	}, "json")
 }
 
 
-/*function ajaxLoadComplain(no) {
-	$.getJSON(serverAddr + "/complain/detail.json?no=" + no, function(obj) {
+function ajaxComplainListRsvd() {
+	$.getJSON(serverAddr + "/complain/list2.json", {
+		"pageNo": pageNo,
+		"length": pageLength
+	}, function(obj) {
 		var result = obj.jsonResult
+		console.log(result.data)
 		if (result.state != "success") {
-			alert("조회 실패입니다.")
-			return
-		}
+	    	 alert("서버에서 데이터를 가져오는데 실패했습니다.")
+	    	 return
+	    }
 		
-		$("#no").val(result.data.no);
-		$("#title").val(result.data.title);
-		$("#contents").val(result.data.contents);
-		$("#createdDate").text(result.data.createdDate2);
-		$("#viewCount").text(result.data.viewCount);
-	})
-}*/
-
-
-//function ajaxLoginUser() {
-//	$.getJSON(serverAddr + "/auth/loginUser.json", function(obj) {
-//		var result = obj.jsonResult
-//	    if (result.state != "success") { // 로그아웃 상태일 경우 로그인 상태와 관련된 태그를 감춘다.
-//	         $('.my-login').css("display", "none")
-//	         return
-//	    }
-//	      
-//	    $('.my-logout').css("display", "none")
-//	      
-//	    $("#userName").text(result.data.name);
-//    })
-//}
-
-
+	    var template = Handlebars.compile($('#trTemplateText2').html())
+	    $("#complainTable2 .kingking").html(template(result.data))
+	  	    
+	    // 현재 페이지 번호를 span 태그에 출력한다.
+	    pageNo = result.data.pageNo;
+	    totalPage = result.data.totalPage;
+	    $('#pageNo_tab2').text(pageNo);
+	    
+	    // 페이지 번호가 1이면 [이전] 버튼을 비활성화시킨다.
+	    if (pageNo <= 1) {
+	    	$('#prevBtn2').attr('disabled', true);
+	    } else {
+	    	$('#prevBtn2').removeAttr('disabled');
+	    } 
+	    
+	    // 페이지 번호가 마지막 페이지라면 [다음] 버튼을 비활성화시킨다.
+	    if (pageNo >= totalPage) {
+	    	$('#nextBtn2').attr('disabled', true);
+	    } else {
+	    	$('#nextBtn2').removeAttr('disabled');
+	    }
+    })
+}
