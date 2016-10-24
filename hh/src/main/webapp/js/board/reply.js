b95d81d262c526f825396ef9a886514b87210782
BitCamp <BitCamp@BitCamp-PC>
2016-10-24 10:49:08 +0900

    세현공지(리플)

--- hh/src/main/webapp/js/board/reply.js
+++ hh/src/main/webapp/js/board/reply.js
@@ -2,45 +2,53 @@ function ajaxReplyList(no) {
 	$.getJSON(serverAddr + "/reply/list2.json?no=" + no, function(obj) {
 		var result = obj.jsonResult
 		if (result.state != "success") {
-	    	 alert("서버에서 데이터를 가져오는데 실패했습니다.")
-	    	 return
-	    }
-		
-	    var template = Handlebars.compile($('#rrTemplateText').html())	    
-	    $("#replyTable tbody").html(template(result))	    
-	    
-    })
-    
-    $(document.body).on("click","#reDeleteBtn",function(event) {	
-    	if (confirm("정말 삭제하시겠습니까?") == true) {
- 		ajaxDeleteReply($(this).attr("data-no"))
-    	} else {
-    		return;
-    	}  
- });
+			alert("서버에서 데이터를 가져오는데 실패했습니다.")
+			return
+		}
+		var template = Handlebars.compile($('#rrTemplateText').html())	    
+		$("#replyTable tbody").html(template(result))	    
+
+	})
+
+	$(document.body).on("click",".bit-delete-btn",function(event) {	
+		if (confirm("정말 삭제하시겠습니까?") == true) {
+			ajaxDeleteReply($(this).attr("data-no"))
+		} else {
+			return;
+		}  
+	});
 }
+
 
 $("#reAddBtn").click(function(event) {	
 	if (location.search.startsWith("?")) {
 		var no = location.search.split("=")[1];
-		}
+	}
 	var reply = {
-	  //email: $("#email").val(),
-	  no: no,	
-	  reContents: $("#reContents").val()
-	  //createdDate: $("#createdDate").val()
-	  /*type: $("#type").val()*/
+			no: no,	
+			reContents: $("#reContents").val()
 	}
 	ajaxAddReply(reply)
 });
+
+
+$(document.body).ready(function() {
+    $('.reAddLimit').on('keyup', function() {
+        if($(this).val().length > 50) {
+        	 alert("글자수는 50자 이내로 제한됩니다.!");  
+            $(this).val($(this).val().substring(0, 50));
+        }
+    });
+});
+
 
 function ajaxAddReply(reply) {
 	$.post(serverAddr + "/reply/add2.json?no=" + no, reply, function(obj) {
 		var result = obj.jsonResult
 		if (result.state != "success") {
-	    	 alert("등록 실패입니다.")
-	    	 return
-	    }	
+			alert("등록 실패입니다.")
+			return
+		}	
 		location.reload();
 	}, "json")
 }
@@ -68,17 +76,15 @@ $(document.body).on("click",".bit-update-btn",function(event) {
 
 $(document.body).on("click",".bit-save-btn",function(event) {
 	var reply = {
-			reno: $("#reno").val(),  
+			reno: $(".saveReno").val(),  
 			reContents: $(".update-contents").val()
-			//email: $(#email).val()
 	}
-	console.log(reply)
 	if (confirm("정말 변경하시겠습니까?") == true) {
 		ajaxUpdateReply(reply)
 	} else {
 		return;
 	}
-	
+
 });
 
 
@@ -90,20 +96,31 @@ function ajaxLoadReply(no) {
 			return
 		} else {
 			$(".update-contents").val(result.data.reContents);
+			$(".saveReno").val(result.data.reno);
 		}
-	})
+	})	
+	
 	$("#replyTable tr[data-no=" + no + "]").find("td:eq(2)").html(
-			"<textarea type='text' class='update-contents'></textarea>");
+	"<textarea type='text' class='update-contents reUpdateLimit'></textarea>");
 	$("#replyTable tr[data-no=" + no + "]").find("td:eq(5)").html(
-			"<button type='button' class='bit-save-btn'>저장</button><button>취소</button>");
+			"<button type='button' class='bit-save-btn saveReno'>저장</button>" +
+	"<button type='button' class='bit-cancel-btn saveReno'>취소</button>");
 }
 
+
+$(document.body).ready(function() {
+    $('.reUpdateLimit').on('keyup', function() {
+        if($(this).val().length > 50) {
+        	 alert("글자수는 50자 이내로 제한됩니다.!");  
+            $(this).val($(this).val().substring(0, 50));
+        }
+    });
+});
 
 
 function ajaxUpdateReply(reply) {
 	$.post(serverAddr + "/reply/update2.json", reply, function(obj) {
 		var result = obj.jsonResult
-		console.log(result)
 		if (result.state != "success") {
 			alert("변경 실패입니다.")
 			return
@@ -113,13 +130,27 @@ function ajaxUpdateReply(reply) {
 }
 
 
+$(document.body).on("click",".bit-cancel-btn",function(event) {
+	var reno = $(".saveReno").val()  
+	ajaxCancelReply(reno)
+});
 
 
-
-
-
-
-
+function ajaxCancelReply(no) {
+	$.getJSON(serverAddr + "/reply/detail2.json?no=" + no, function(obj) {
+		var result = obj.jsonResult		
+		if (result.state != "success") {
+			alert("조회 실패입니다.")
+			return
+		} else {			
+			$("#replyTable tr[data-no=" + no + "]").find("td:eq(2)").html(
+					result.data.reContents);
+			$("#replyTable tr[data-no=" + no + "]").find("td:eq(5)").html(
+					"<button type='button' class='btn btn-primary btn-sm bit-update-btn' data-no="+ no +">수정</button>" +
+					"<button type='button' class='btn btn-primary btn-sm bit-delete-btn' data-no="+ no +">삭제</button>");	
+		}
+	})		
+}
 
 
 
