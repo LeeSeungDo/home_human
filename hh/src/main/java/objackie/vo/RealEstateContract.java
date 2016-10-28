1627005e0437aa8224ed4acbb917a3b4c68fe0ca
BitCamp <BitCamp@BitCamp-PC>
2016-10-28 16:49:55 +0900

    .

--- hh/src/main/java/objackie/vo/RealEstateContract.java
+++ hh/src/main/java/objackie/vo/RealEstateContract.java
@@ -3,7 +3,7 @@ package objackie.vo;
 import java.io.Serializable;
 import java.sql.Date;
 
-public class Contract implements Serializable {
+public class RealEstateContract implements Serializable {
   private static final long serialVersionUID = 1L;
   
   protected int contractNo;
