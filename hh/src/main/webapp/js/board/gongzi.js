becfef35f63cfb6f9fab81f9932932e8d4b12b63
BitCamp <BitCamp@BitCamp-PC>
2016-11-06 16:06:06 +0900

    세현공지(페이징)

--- hh/src/main/webapp/js/board/gongzi.js
+++ hh/src/main/webapp/js/board/gongzi.js
@@ -13,8 +13,23 @@ function ajaxFirstList() {
 
 
 
+$("#prevBtn").click(function(event) {
+	pageNo--;
+	ajaxGongziList();
+});
+
+$("#nextBtn").click(function(event) {
+	pageNo++;
+	ajaxGongziList();
+});
+
+
+// 글로벌 변수 = window 프로퍼티 
+var pageNo = 1, /* window.pageNo */
+    pageLength = 6; /* window.pageLength */
+
 function ajaxGongziList() {
-	$.getJSON(serverAddr + "/board/list.json", function(obj) {
+	$.getJSON(serverAddr + "/board/list.json", {"pageNo": pageNo, "length": pageLength}, function(obj) {
 		var result = obj.jsonResult
 		if (result.state != "success") {
 	    	 alert("서버에서 데이터를 가져오는데 실패했습니다.")
@@ -26,9 +41,43 @@ function ajaxGongziList() {
 	    $(document.body).on('click', '.card1', function(event) {
 		    window.location.href = serverAddr + "/html/board/gongziForm.html?no=" + $(this).attr("data-no")
 	    })
+	    
+	    // 현재 페이지 번호를 span 태그에 출력한다.
+	    pageNo = result.data.pageNo;
+	    totalPage = result.data.totalPage;
+	    $('#pageNo').text(pageNo);
+	    
+	    // 페이지 번호가 1이면 [이전] 버튼을 비활성화시킨다.
+	    if (pageNo <= 1) {
+	    	$('#prevBtn').attr('disabled', true);
+	    } else {
+	    	$('#prevBtn').removeAttr('disabled');
+	    } 
+	    
+	    // 페이지 번호가 마지막 페이지라면 [다음] 버튼을 비활성화시킨다.
+	    if (pageNo >= totalPage) {
+	    	$('#nextBtn').attr('disabled', true);
+	    } else {
+	    	$('#nextBtn').removeAttr('disabled');
+	    }
     })
 }
 
+
+
+$("#prevBtnT").click(function(event) {
+	pageNoT--;
+	ajaxGongziList_T();
+});
+
+$("#nextBtnT").click(function(event) {
+	pageNoT++;
+	ajaxGongziList_T();
+});
+
+//글로벌 변수 = window 프로퍼티 
+var pageNoT = 1, /* window.pageNo */
+    pageLengthT = 6; /* window.pageLength */
 
 function ajaxGongziList_T() {
 	$.getJSON(serverAddr + "/board/list.json", function(obj) {
@@ -43,6 +92,25 @@ function ajaxGongziList_T() {
 	    $(document.body).on('click', '.card1', function(event) {
 		    window.location.href = serverAddr + "/html/board/gongziForm_t.html?no=" + $(this).attr("data-no")
 	    })
+	    
+	    // 현재 페이지 번호를 span 태그에 출력한다.
+	    pageNoT = result.data.pageNo;
+	    totalPageT = result.data.totalPage;
+	    $('#pageNoT').text(pageNo);
+	    
+	    // 페이지 번호가 1이면 [이전] 버튼을 비활성화시킨다.
+	    if (pageNoT <= 1) {
+	    	$('#prevBtnT').attr('disabled', true);
+	    } else {
+	    	$('#prevBtnT').removeAttr('disabled');
+	    } 
+	    
+	    // 페이지 번호가 마지막 페이지라면 [다음] 버튼을 비활성화시킨다.
+	    if (pageNoT >= totalPageT) {
+	    	$('#nextBtnT').attr('disabled', true);
+	    } else {
+	    	$('#nextBtnT').removeAttr('disabled');
+	    }
     })
 }
 
