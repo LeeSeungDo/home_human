b44f249ed36fbcf421155ec41b1cc3e54acb3fa7
BitCamp <BitCamp@BitCamp-PC>
2016-11-06 17:16:26 +0900

    세현 계약관리

--- hh/src/main/java/objackie/dao/FreeBoardFileDao.java
+++
@@ -1,8 +0,0 @@
-package objackie.dao;
-
-import objackie.vo.FreeBoardFile;
-
-public interface FreeBoardFileDao {
-  int insert(FreeBoardFile freeboardFile);
-  int delete(int no) throws Exception;
-}
