ad62616d69884a1c4d7983ed365739136f0f6d41
BitCamp <BitCamp@BitCamp-PC>
2016-11-06 14:26:29 +0900

    공지 세현(파일업로드 완료)

--- hh/src/main/webapp/js/board/gongziForm.js
+++ hh/src/main/webapp/js/board/gongziForm.js
@@ -21,7 +21,7 @@ function ajaxLoadGongzi(no) {
 		$("#contents").text(result.data.contents);
 		$("#createdDate").text(result.data.createDate);
 		$("#viewCount").text(result.data.viewCount);
-		$("#file1").attr('src', "../upload/" + result.data.filename);
+		$("#file1").attr('src', "../../upload/" + result.data.filename);
 		$("#updateBtn").click(function(event) {
 			window.location.href = serverAddr + "/html/board/gongziUpdate.html?no=" + no
 		})
