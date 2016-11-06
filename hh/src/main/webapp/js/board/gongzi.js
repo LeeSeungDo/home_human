00573c1a81a3abbd4c2c4a85e698848aa24e4588
BitCamp <BitCamp@BitCamp-PC>
2016-11-06 12:57:26 +0900

    세현(공지 임차인)

--- hh/src/main/webapp/js/board/gongzi.js
+++ hh/src/main/webapp/js/board/gongzi.js
@@ -30,6 +30,23 @@ function ajaxGongziList() {
 }
 
 
+function ajaxGongziList_T() {
+	$.getJSON(serverAddr + "/board/list.json", function(obj) {
+		var result = obj.jsonResult
+		if (result.state != "success") {
+	    	 alert("서버에서 데이터를 가져오는데 실패했습니다.")
+	    	 return
+	    }
+	    var template = Handlebars.compile($('#trTemplateText').html())	    
+	    $("#gongziTable").html(template(result.data))
+	    
+	    $(document.body).on('click', '.card1', function(event) {
+		    window.location.href = serverAddr + "/html/board/gongziForm_t.html?no=" + $(this).attr("data-no")
+	    })
+    })
+}
+
+
 
 
 
