fc61d99bf66bbc5f4c34baa78e3eb76ae858585e
BitCamp <BitCamp@BitCamp-PC>
2016-11-06 17:14:48 +0900

    세현 입주자 관리

--- hh/src/main/java/objackie/controller/RealEstateContractController.java
+++ hh/src/main/java/objackie/controller/RealEstateContractController.java
@@ -3,19 +3,26 @@
 import java.util.HashMap;
 import java.util.List;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import objackie.service.RealEstateContractService;
 import objackie.vo.RealEstateContract;
+import objackie.vo.Board;
 import objackie.vo.JsonResult;
 
 @Controller 
 @RequestMapping("/contract/")
 public class RealEstateContractController {
   
+  @Autowired ServletContext sc;
   @Autowired RealEstateContractService realEstateContractService;
      
   @RequestMapping(path="list1")
@@ -79,11 +86,17 @@ public class RealEstateContractController {
   }
   
   @RequestMapping(path="add")
-  public Object add(RealEstateContract realEstateContract) throws Exception {
+  @ResponseBody
+  public Object add(@ModelAttribute RealEstateContract realEstateContract, MultipartFile file) throws Exception {
+    String uploadDir = sc.getRealPath("/upload") + "/";
+    System.out.println("-----------------------파일 업로드--------------------------------");
+    System.out.println(file);
+    System.out.println(uploadDir);
+    System.out.println("-----------------------/파일 업로드--------------------------------");
     try {
-      realEstateContractService.insertRealEstateContract(realEstateContract, null, null, null);
+      realEstateContractService.insertRealEstateContract(realEstateContract, file, uploadDir);
       return JsonResult.success();
-      
+
     } catch (Exception e) {
       return JsonResult.fail(e.getMessage());
     }
