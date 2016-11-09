99ddc8edfae9bbd1bbd6eb54a32fb37c2fbd1350
SOO <SOO@192.168.0.216>
2016-11-09 17:41:40 +0900

    윤인수 - 대시보드 링크 수정 (myinfo) / muyinfo 대시보드 추가

--- hh/src/main/webapp/js/auth/myinfo.js
+++ hh/src/main/webapp/js/auth/myinfo.js
@@ -25,7 +25,7 @@ $("#memberUpBtn").click(function(event) {
 
 
 
-// 회원정보수정 회원데이터 출력
+//회원정보수정 회원데이터 출력
 function ajaxLoginUser() {
 	$.getJSON(serverAddr + "/auth/loginUser.json", function(obj) {
 		var result = obj.jsonResult
@@ -33,7 +33,7 @@ function ajaxLoginUser() {
 			window.location.href = serverAddr + "/html/index.html"
 			return
 		}
-		
+
 		$("#email").html(result.data.email);
 		$("#name").html(result.data.name);
 		$("#gender").html(result.data.gender);
@@ -43,13 +43,88 @@ function ajaxLoginUser() {
 		$("#detailAddr").html(result.data.detailAddr);
 		$("#tel").html(result.data.tel);
 		$("#phoPath").html(result.data.phoPath);
+		$("#userName1").html(result.data.name);
+		$("#userName2").html(result.data.name);
+		$("authLevel").html(result.data.name);
 
+		var auth = result.data.auth;
 		var gender = result.data.gender;
-	    if (gender == 0) {
-	    	
-	    	$("#gender").html("남자");
-	    } else {
-	    	$("#gender").html("여자");
-	    }
-    })
+
+		if (auth == 0) {
+			$("#authLevel").html("임대인");
+		} else {
+			$("#authLevel").html("임차인");
+		}
+		
+		if (gender == 0) {
+			$("#gender").html("남자");
+		} else {
+			$("#gender").html("여자");
+		}
+	})
 }
+
+$("#myInfo").click(function(event) {
+	alert("내정보");
+	window.location.href = serverAddr + "/html/auth/myinfo.html"
+});
+
+
+
+$(document).ready(function() {
+	Kakao.init("bfb48672ff68dbf137c2daffb44adfb0");
+
+	$("#logout").click(function(event) {
+		alert("로그아웃");
+		Kakao.Auth.logout(function(data) {
+			if (data) { // 정상적으로 로그아웃이 되면 true가 떨어져서 처리하였습니다.
+				window.location.href = serverAddr + "/html/index.html";
+			}
+		});
+	});
+
+	$("#getOut").click(function(event) {
+		alert("탈퇴");
+		Kakao.API.request({
+			url : '/v1/user/unlink',
+			success : function(res) {
+				alert("탈퇴성공");
+				kakaoOut();
+				//window.location.href = serverAddr + "/html/index.html";
+			}
+		});
+	});
+
+	function kakaoOut() {
+		alert("로그아웃 중...");
+		Kakao.Auth.logout(function(data) {
+			if (data) { // 정상적으로 로그아웃이 되면 true가 떨어져서 처리하였습니다.
+				alert("로그아웃 됨!");
+				window.location.href = serverAddr + "/html/index.html";
+			}
+		});
+	}
+});
+
+
+///*----------------------------------------------------- 로그인 정보 불러오기 -----------------------------------------------------*/
+//function ajaxLoginUser() {
+//$.getJSON(serverAddr + "/auth/loginUser.json", function(obj) {
+//var result = obj.jsonResult
+//if (result.state != "success") { // 로그아웃 상태일 경우 로그인 상태와 관련된 태그를 감춘다.
+//window.location.href = serverAddr + "/html/index.html"
+//return
+//}
+
+//$("#userName1").html(result.data.name);
+//$("#userName2").html(result.data.name);
+
+//var auth = result.data.auth;
+//if (auth == 0) {
+//$("#authLevel").html("임대인");
+//} else {
+//$("#authLevel").html("[임차인]");
+//}
+//})
+//}
+///*----------------------------------------------------- /로그인 정보 불러오기 -----------------------------------------------------*/
