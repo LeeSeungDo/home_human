f291db928275b287bfaa504c5335fb930f12481d
Tessa <Tessa@DESKTOP-J658CSB>
2016-11-13 15:10:02 +0900

    .

--- hh/src/main/webapp/js/board/complainUpdate_t.js
+++ hh/src/main/webapp/js/board/complainUpdate_t.js
@@ -1,3 +1,12 @@
+$(document.body).ready(function() {
+    $('.limitation').on('keyup', function() {
+        if($(this).val().length > 200) {
+        	 alert("글자수는 200자 이내로 제한됩니다.!");  
+            $(this).val($(this).val().substring(0, 200));
+        }
+    });
+});
+
 var rsvdAll;
 
 function rsvdCall(rsvd) {
@@ -57,6 +66,22 @@ function ajaxUpdateComplain(complain) {
 		}
 		window.location.href = "complain_t.html"
 	}, "json")
+}
+
+
+function ajaxLoginUser() {
+	$.getJSON(serverAddr + "/auth/loginUser.json", function(obj) {
+		var result = obj.jsonResult
+	    if (result.state != "success") { // 로그아웃 상태일 경우 로그인 상태와 관련된 태그를 감춘다.
+	    	alert("로그아웃 되었습니다.");
+	    	window.location.href = serverAddr + "/html/index.html"
+	         return
+	    }
+
+		$(".email").val(result.data.email);
+		$(".writer").val(result.data.name);
+		
+    })
 }
 
 function ajaxDeleteComplain(no) {
