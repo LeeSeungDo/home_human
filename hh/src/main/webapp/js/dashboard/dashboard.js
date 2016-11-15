2f4306fe35660d3159b834ac5c768aba7e80e040
BitCamp <BitCamp@BitCamp-PC>
2016-11-15 14:12:39 +0900

    세현(건물정보 뿌리기)

--- hh/src/main/webapp/js/dashboard/dashboard.js
+++ hh/src/main/webapp/js/dashboard/dashboard.js
@@ -162,7 +162,23 @@ function ajaxTenantList() {
 }
 /*----------------------------------------------------- /세입자 불러오기 -----------------------------------------------------*/
 
+function ajaxBuildList() {
+	$.getJSON(serverAddr + "/build/list.json", function(obj) {
+		var result = obj.jsonResult
+		console.log(result)
+		if (result.state != "success") {
+			alert("서버에서 데이터를 가져오는데 실패했습니다.")
+			return
+		}
 
+		var template = Handlebars.compile($('#buildList').html())	    
+		$("#buildListTable").html(template(result.data))		
+		
+		$(document.body).on('click', '.contractForm1', function(event) {
+		window.location.href = serverAddr + "/html/contract/viewMyContract.html?no=" + $(this).attr("data-no")
+		})
+	})
+}
 
 
 
